cff1ec2b77817e82d2e3fb6ba7db2ad63bd6e48b
Matthew Bowler <mbowler03@gmail.com>
2020-06-24 08:12:45 -0400

    Updates

---
+++ precache-manifest.67051ed7d69bd585eb6c2a25f4bcdd94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281d384de58fe2e6eb19ce13b6de28ad",
+    "url": "/travel-memory-game/index.html"
+  },
+  {
+    "revision": "ff3ce28464c790da5625",
+    "url": "/travel-memory-game/static/css/main.caff8d27.chunk.css"
+  },
+  {
+    "revision": "0c930e78289b01f21a95",
+    "url": "/travel-memory-game/static/js/2.0e3fa562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel-memory-game/static/js/2.0e3fa562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3ce28464c790da5625",
+    "url": "/travel-memory-game/static/js/main.b56843d4.chunk.js"
+  },
+  {
+    "revision": "61bf344ebc9593a6518b",
+    "url": "/travel-memory-game/static/js/runtime-main.a25778df.js"
+  }
+]);
